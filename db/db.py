8952717f7ab0d90d17c29f93247eeab16bfd6f0c
Zac Leids <zacleids@gmail.com>
2025-04-13 14:01:15 -0700

    add config.py and use new config

--- db/db.py
+++ db/db.py
@@ -1,12 +1,9 @@
 import os
 from peewee import SqliteDatabase
+from config import Config
 
-# Get the absolute path to ensure it works no matter where the script runs
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_NAME = os.path.join(BASE_DIR, "bot_orm.db")   # Ensures it's in `db/`
-
-orm_db = SqliteDatabase(DB_NAME)
-
+config = Config()
+orm_db = SqliteDatabase(config.db_orm_path)
 
 import time_funcs
 import todo
@@ -19,11 +16,9 @@ from daily_checklist import DailyChecklist, DailyChecklistCheck
 def create_dbs():
     todo.create_db()
 
-    orm_db_file_path = "bot_orm.db"
-
     # Ensure DB file exists
-    if not os.path.exists(DB_NAME):
-        open(DB_NAME, 'a').close()
+    if not os.path.exists(config.db_orm_path):
+        open(config.db_orm_path, 'a').close()
         print("DB Created!")
     else:
         print("DB already exists")
